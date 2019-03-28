88fff1b1573c38254c52f1fd7ac84c76fe2dd353
Unknown <mrizo036@west-mec.org>
2019-03-27 18:28:12 -0700

    Code Completed
    All HTML and JavaScript code completed. Comments must be added.

--- pc_cword.js
+++ pc_cword.js
@@ -48,6 +48,7 @@
 
 
 */
+//The block below creates a number of global variables which will be utilized later within the code functions. 
 var allLetters;
 var currentLetter;
 var wordLetters;
@@ -55,6 +56,7 @@ var acrossClue;
 var downClue;
 var typeDirection = "right";
 
+//The line below states that when the page is loaded in the browser, the init function will be run.
 window.onload = init;
 
 function init() {
@@ -64,35 +66,39 @@ function init() {
       var downID = currentLetter.dataset.clueD;
       acrossClue = document.getElementById(currentLetter.dataset.clueA);
       downClue = document.getElementById(currentLetter.dataset.clueD);
- 
+      //This line calls the formatPuzzle function
       formatPuzzle(currentLetter);
 
       for (var i = 0; i < allLetters.length; i++) {
             allLetters[i].style.cursor = "pointer";
-            allLetters[i].onmousedown = function(e){
+            allLetters[i].onmousedown = function (e) {
                   formatPuzzle(e.target);
             };
       }
-
+      //The line bellow states that when the a key is pressed down on the keyboard, the selectLetter function will be run. 
       document.onkeydown = selectLetter;
-
+      //This line creates a variable with the name of typeImage. The variable is then given the vallue of the element with an if of directionImg.
       var typeImage = document.getElementById("directionImg");
+
       typeImage.style.cursor = "pointer";
       typeImage.onclick = switchTypeDirection;
 
-      document.getElementById("showErrors").onclick = function (){
+      document.getElementById("showErrors").onclick = function () {
+            //The for statement below states that the code block within will continue to run as long as the variale i is a value less than the length of the allLeters array.
             for (var i = 0; i < allLetters.length; i++) {
-                  if (allLetters[i].textContent !== allLetters[i].dataset.letter){
+                  //The if statement below states that when the text content of the allLetters array does not equal 
+                  if (allLetters[i].textContent !== allLetters[i].dataset.letter) {
                         allLetters[i].style.color = "red";
-                        setTimeout(function() {
+                        setTimeout(function () {
                               for (var i = 0; i < allLetters.length; i++) {
                                     allLetters[i].style.color = "";
                               }
                         }, 3000);
-                  }   
+                  }
             }
       }
-      document.getElementById("showSolution").onclick = function() {
+      //The document 
+      document.getElementById("showSolution").onclick = function () {
             for (var i = 0; i < allLetters.length; i++) {
                   allLetters[i].textContent = allLetters[i].dataset.letter;
             }
@@ -112,70 +118,77 @@ function formatPuzzle(puzzleLetter) {
             acrossClue = document.getElementById(currentLetter.dataset.clueA);
             acrossClue.style.color = "blue";
             wordLetters = document.querySelectorAll("[data-clue-a = " + currentLetter.dataset.clueA + "]");
-            
+
             for (var i = 0; i < wordLetters.length; i++) {
                   wordLetters[i].style.backgroundColor = "rgb(231, 231, 255)";
-                  
+
             }
       }
 
       if (currentLetter.dataset.clueD !== undefined) {
             downClue = document.getElementById(currentLetter.dataset.clueD);
             downClue.style.color = "red";
-            wordLetters = document.querySelectorAll("[data-clue-d = "+currentLetter.dataset.clueD+"]");
-            
+            wordLetters = document.querySelectorAll("[data-clue-d = " + currentLetter.dataset.clueD + "]");
+
             for (var i = 0; i < wordLetters.length; i++) {
                   wordLetters[i].style.backgroundColor = "rgb(255, 231, 231)";
-                  
+
             }
       }
-      
 
-      if (typeDirection = "right") {
-            currentLetter.style.backgroundColor= "rgb(191, 191, 255)";
+
+      if (typeDirection === "right") {
+            currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
       } else {
             currentLetter.style.backgroundColor = "rgb(255, 191, 191)";
       }
 }
 
-function selectLetter(e){
-      var leftLetter = currentLetter.dataset.left;
-      var upLetter = currentLetter.dataset.up;
-      var rightLetter = currentLetter.dataset.right;
-      var downLetter = currentLetter.dataset.down;
+//The code block 
+function selectLetter(e) {
+      var leftLetter = document.getElementById(currentLetter.dataset.left);
+      var upLetter = document.getElementById(currentLetter.dataset.up);
+      var rightLetter = document.getElementById(currentLetter.dataset.right);
+      var downLetter = document.getElementById(currentLetter.dataset.down);
 
       var userKey = e.keyCode;
-
-      if(userKey == 37){
+      //The if statement block below provides commands for how the crossword should react when the user uses the keyboard keys. For each key on the keyboard, the page will react differently by moving the users cursor or writing a letter into the space. 
+      if (userKey === 37) {
             formatPuzzle(leftLetter);
-      } else if (userKey == 18){
+      } else if (userKey === 38) {
             formatPuzzle(upLetter);
-      } else if (userKey == 39 || userKey == 9){
+      } else if (userKey === 39 || userKey === 9) {
             formatPuzzle(rightLetter);
-      } else if (userKey == 40 || userKey == 13){
+      } else if (userKey === 40 || userKey === 13) {
             formatPuzzle(downLetter);
-      } else if (userKey == 8 || userKey == 46){
-            currentLetter = "";
-      } else if (userKey == 32){
+      } else if (userKey === 8 || userKey === 46) {
+            currentLetter.textContent = "";
+      } else if (userKey === 32) {
             switchTypeDirection();
-      } else if(65<userKey<90){
-            currentLetter = getChar(userKey);
-      } else if (typeDirection == "right"){
-            formatPuzzle(rightLetter);
-      } else {
-            formatPuzzle(downLetter);
+      } else if (userKey >= 65 && userKey <= 90) {
+            currentLetter.textContent = getChar(userKey);
+            if (typeDirection === "right") {
+                  formatPuzzle(rightLetter);
+            } else {
+                  formatPuzzle(downLetter);
+            }
       }
-
-      preventDefault(KeyboardEvent);
+      //The line beloe 
+      e.preventDefault();
 }
-
-function switchTypeDirection(){
+//The block below creates a function with the name of switchTypeDirection. The functions is not given any parameters.
+function switchTypeDirection() {
+      //
       var typeImage = document.getElementById("directionImg");
 
-      if (typeDirection == "right"){
+      if (typeDirection === "right") {
             typeDirection = "down";
+            typeImage.src = "pc_down.png";
+            currentLetter.style.backgroundColor = "rgb(255, 191, 191)";
+      } else {
+            typeDirection = "right";
             typeImage.src = "pc_right.png";
-
+            currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
       }
 }
 
